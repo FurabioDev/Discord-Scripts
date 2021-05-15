e6178c67ea086cc84c6bf2942e4511f2ab894992
Furabio <79111258+FurabioDev@users.noreply.github.com>
2021-05-15 21:29:03 +0200

    Create GetToken.js

---
+++ GetToken.js
@@ -0,0 +1,1 @@
+Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}},[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getToken!==void 0).exports.default.getToken()
