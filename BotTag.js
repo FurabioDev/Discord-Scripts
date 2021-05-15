d480042f22fb013435a6c205bcc8f1a804258f0e
Furabio <79111258+FurabioDev@users.noreply.github.com>
2021-05-15 21:28:42 +0200

    Create BotTag.js

---
+++ BotTag.js
@@ -0,0 +1,2 @@
+var findModule = (item) => Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}}, [['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default[item]!==void 0).exports.default;
+findModule('getCurrentUser').getCurrentUser().bot = true;
