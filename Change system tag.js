a9046b5bb17bb7284200f61a90fb79cda77ce36a
Furabio <79111258+FurabioDev@users.noreply.github.com>
2021-05-15 21:27:46 +0200

    Create Change system tag.js

---
+++ Change system tag.js
@@ -0,0 +1,3 @@
+var findModule = (item) => Object.values(webpackJsonp.push([[],{['']:(_,e,r)=>{e.cache=r.c}}, [['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default[item]!==void 0).exports.default;
+
+findModule('Messages').Messages.SYSTEM_DM_TAG_SYSTEM = 'System Tag Name Here';
